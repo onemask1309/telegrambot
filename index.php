8b4f2175a39d334c6680958018b4086df9c5e445
onemask1309 <77883479+onemask1309@users.noreply.github.com>
2021-04-08 15:53:01 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+$token = "1773281553:AAGPbMMMvMbVfXj7bhXUA1uU8i5xi7nlHoQ";
+$link1 = "https://api.telegram.org/bot".$token;
+
+$updates = file_get_contents('php://input');
+$updates = json_decode($updates, TRUE);
+
+$msgID = $updates['message']['chat']['id'];
+$name = $updates['message']['chat']['first_name'];
+$text = $updates['message']['text'];
+
+switch ($text) {
+	case"start":
+		sendmsg($msgID, "Welcome $name");
+		break;
+	case"bye":
+	sendmsg($msgID, "Nice To See You");
+	sendmsg($msgID, "\xF0\x9F\x98\x98");
+	break;
+	
+}
+function sendmsg($msgID, $text)
+{
+	$url = $GLOBALS[link1].'/sendMessage?chat_id='.$msgID.'&text='.urlencode($text);
+	file_get_contents($url);
+}
+?>
